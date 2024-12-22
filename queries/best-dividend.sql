b4178c7d7e2e564ebd35fe55bcfe52dac76f5802
Scott Campbell <scottcampbell242@gmail.com>
2024-12-22 18:22:33 -0500

    Update best-dividend.sql

--- queries/best-dividend.sql
+++ queries/best-dividend.sql
@@ -1,1 +1,14 @@
-
+SELECT
+    DATE_TRUNC('year', Date) AS Year,
+    SUM(Dividend) AS Total_Dividend,
+    'Coca-Cola' AS Company
+FROM KO_stock_dividend
+GROUP BY Year
+UNION ALL
+SELECT
+    DATE_TRUNC('year', Date) AS Year,
+    SUM(Dividend) AS Total_Dividend,
+    'PepsiCo' AS Company
+FROM PEP_stock_dividend
+GROUP BY Year
+ORDER BY Total_Dividend DESC;
